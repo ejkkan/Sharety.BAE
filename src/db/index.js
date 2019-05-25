25633fac33b68d5cd26c1631b7578a4378e767e4
ejkkan <erikper.magnusson@gmail.com>
2019-05-25 15:24:21 +0200

    dsfs

--- src/db/index.js
+++ src/db/index.js
@@ -5,25 +5,19 @@ export let client = null;
 
 export default async () => {
   try {
-    // const dbUrl = process.env.production
-    //   ? process.env.PROD_DB
-    //   : process.env.DEV_DB;
-    // const dbUrl =
-    //   "mongodb://admin:T2fOEtAmGddNL9Ae@SG-playground-20964.servers.mongodirector.com:48469,SG-playground-20965.servers.mongodirector.com:48469,SG-playground-20966.servers.mongodirector.com:48469/admin?replicaSet=RS-playground-0&ssl=true";
-    const dbUrl = process.env.LOCAL_DB;
-    client = await MongoClient.connect(
-      "mongodb://127.0.0.1:27017/graphql-development",
-      {
-        //useCreateIndex: true,
-        useNewUrlParser: true
-      }
-    );
+    const dbUrl = process.env.production
+      ? process.env.PROD_DB
+      : process.env.DEV_DB;
+    client = await MongoClient.connect(dbUrl, {
+      //useCreateIndex: true,
+      useNewUrlParser: true
+    });
 
-    // const dbName = process.env.production
-    //   ? process.env.PROD_DB_NAME
-    //   : process.env.DEV_DB_NAME;
+    const dbName = process.env.production
+      ? process.env.PROD_DB_NAME
+      : process.env.DEV_DB_NAME;
 
-    const db = client.db("graphql-development");
+    const db = client.db(dbName);
     client.get;
     db.on("error", function(err) {
       const message = "MongoDB close" + (err ? ": " + err.message : "");
--- src/index.js
+++ src/index.js
@@ -5,31 +5,6 @@ import startDB from "./db";
 import attachUserCredentials from "./middlewares/attachUserCredentials";
 import schema from "./graphql";
 
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString: process.env.DB
-});
-const text = "INSERT INTO items(text) VALUES($1) RETURNING *";
-const values = ["brianc"];
-
-// pool.query(
-//   "CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null)"
-// );
-pool.query(text, values, (err, res) => {
-  if (err) {
-    console.log(err.stack);
-  } else {
-    console.log(res.rows[0]);
-    // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-  }
-});
-
-pool.query("SELECT NOW()", (err, res) => {
-  console.log(err, res);
-  pool.end();
-});
-
 async function start() {
   const { db, collections, ObjectID } = await startDB();
   //const pubSub = new PubSub()
@@ -57,13 +32,15 @@ async function start() {
         credentials: true,
         origin: ["http://localhost:3000"] // here define the origins
       },
-      port: 4000,
+      port: process.env.PORT || 4000,
       endpoint: "/graphql",
       subscriptions: "/subscriptions",
       playground: "/playground"
     },
     () => {
-      console.log(`Server is running on http://localhost:${4000}`);
+      console.log(
+        `Server is running on http://localhost:${process.env.PORT || 4000}`
+      );
     }
   );
 }
